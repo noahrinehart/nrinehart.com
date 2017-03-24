692c6ff95c20841dcea20ec9020e43fe1c920e50
Noah Rinehart <rinehart.noah@gmail.com>
2017-03-24 17:52:33 +0000

    added purdue

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
+#!/usr/bin/env nodejs
 var express = require('express');
 var favicon = require('serve-favicon');
 var path = require('path');
 
 var app = express();
 
+const PORT = 8080;
+
 app.use(favicon(path.join(__dirname, 'public/assets/images/favicon.ico')));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(3000, function () {
-  console.log('Listening on port 3000!');
+app.listen(PORT, function () {
+  console.log('Listening on port ' + PORT  + '!');
 });
