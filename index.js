da44b9712ae9109e45cfa4930538dc22802a0a1d
Noah Rinehart <rinehart.noah@gmail.com>
2017-06-10 15:38:14 -0700

    some router test work

--- index.js
+++ index.js
@@ -6,52 +6,37 @@ var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
 var Bear = require('./app/models/bear');
-
-var app = express();
-
-var router = express.Router();
-
-router.use( (req, res, next) => {
-	console.log('Middleware');
-	next();
-});
-
-router.route('/bears')
-	
-	.post( (req, res) => {
-		var bear = new Bear();
-		bear.name = req.body.name;
-		bear.save( (err) => {
-			if (err) res.send(err);
-
-			res.json( {message: 'Bear created!' });
-		});
-	})
-	.get( (req, res) => {
-		Bear.find( (err, bears) => {
-			if (err) res.send(err);
-			res.json(bears);
-		});
-	});
-
-router.get('/', (req, res) => {
-	res.json({ message: 'hello, world' });
-});
-
-mongoose.connect('mongodb://test:mowgli11@ds059365.mlab.com:59365/noahrinehart');
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-app.use(favicon(path.join(__dirname, 'public/assets/images/favicon.ico')));
-//app.use(express.static(path.join(__dirname, 'public')));
+var router = require('./app/routes');
 
 const PORT = process.env.PORT || 8080;
 
+var app = express();
+
+// Parse application/json
+app.use(bodyParser.json());
+
+// Parse application/x-www-form-urlencode
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Set up static files in /public
+// app.use(express.static(path.join(__drrname + '/public')));
+
+// Server static website
 app.use('/', express.static(path.join(__dirname, 'public')));
 
+// Set up favicon
+app.use(favicon(path.join(__dirname, 'public/assets/images/favicon.ico')));
+
+// Config routes
 app.use('/api', router);
 
 app.listen(PORT, () => {
-  console.log('Listening on port ' + PORT  + '!');
+    console.log('Listening on port ' + PORT  + '!');
 });
+
+module.exports = app;
+
+mongoose.connect('mongodb://test:mowgli11@ds059365.mlab.com:59365/noahrinehart');
+
+
+
